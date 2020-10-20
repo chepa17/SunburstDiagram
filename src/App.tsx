79adca2e2909ae0118041c9feeedc1bcab240d2c
chepa <mykola.chepkyi@youshido.com>
2020-10-20 15:44:25 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,86 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import * as am4core from "@amcharts/amcharts4/core";
+// import * as am4charts from "@amcharts/amcharts4/charts";
+import * as am4plugins_sunburst from "@amcharts/amcharts4/plugins/sunburst"; 
+
+let chart = am4core.create("chartdiv", am4plugins_sunburst.Sunburst); 
+
+chart.data = [{
+  name: "First",
+  children: [
+    { name: "A1", value: 100 },
+    { name: "A2", value: 60 }
+  ]
+},
+{
+  name: "Second",
+  children: [
+    { name: "B1", value: 135 },
+    { name: "B2", value: 98 }
+  ]
+},
+{
+  name: "Third",
+  children: [
+    {
+      name: "C1",
+      children: [
+        { name: "EE1", value: 130 },
+        { name: "EE2", value: 87 },
+        { name: "EE3", value: 55 }
+      ]
+    },
+    { name: "C2", value: 148 },
+    {
+      name: "C3", children: [
+        { name: "CC1", value: 53 },
+        { name: "CC2", value: 30 }
+      ]
+    },
+    { name: "C4", value: 26 }
+  ]
+},
+{
+  name: "Fourth",
+  children: [
+    { name: "D1", value: 415 },
+    { name: "D2", value: 148 },
+    { name: "D3", value: 89 }
+  ]
+},
+{
+  name: "Fifth",
+  children: [
+    {
+      name: "E1",
+      children: [
+        { name: "EE1", value: 33 },
+        { name: "EE2", value: 40 },
+        { name: "EE3", value: 89 }
+      ]
+    },
+    {
+      name: "E2",
+      value: 148
+    }
+  ]
+}];
+
+// Define data fields
+chart.dataFields.value = "value";
+chart.dataFields.name = "name";
+chart.dataFields.children = "children";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <script src="//cdn.amcharts.com/lib/4/core.js"></script>
+      <script src="//cdn.amcharts.com/lib/4/charts.js"></script>
+      <script src="//cdn.amcharts.com/lib/4/plugins/sunburst.js"></script>
+      <script src="//cdn.amcharts.com/lib/4/themes/animated.js"></script>
+      <div id="chartdiv"></div>
     </div>
   );
 }
