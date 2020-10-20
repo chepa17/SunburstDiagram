8e1f2273455cb6155a1eb2f13f23cc39c906a2e5
chepa <mykola.chepkyi@youshido.com>
2020-10-20 16:03:36 +0300

    Updates

---
+++ precache-manifest.855539b553b91ef61b9f48044c6a0de0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "624b6194845d87297d382c171799e339",
+    "url": "/chepa17/SunburstDiagram/index.html"
+  },
+  {
+    "revision": "cd9cf05bf5000c8d35eb",
+    "url": "/chepa17/SunburstDiagram/static/css/main.5b0948eb.chunk.css"
+  },
+  {
+    "revision": "cdbf7e84c0504c76ac8d",
+    "url": "/chepa17/SunburstDiagram/static/js/3.07687aef.chunk.js"
+  },
+  {
+    "revision": "bf87ac96dc2e6e512d82",
+    "url": "/chepa17/SunburstDiagram/static/js/4.70de45cf.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "/chepa17/SunburstDiagram/static/js/4.70de45cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "377d0d3a35ef9be6c0a5",
+    "url": "/chepa17/SunburstDiagram/static/js/5.af6dc293.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/chepa17/SunburstDiagram/static/js/5.af6dc293.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7c71f86d203133f457d",
+    "url": "/chepa17/SunburstDiagram/static/js/6.2b856134.chunk.js"
+  },
+  {
+    "revision": "e3b93feea552d5fd932a1758b1da5f1a",
+    "url": "/chepa17/SunburstDiagram/static/js/6.2b856134.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd9cf05bf5000c8d35eb",
+    "url": "/chepa17/SunburstDiagram/static/js/main.56b26219.chunk.js"
+  },
+  {
+    "revision": "79c44749448afd923290",
+    "url": "/chepa17/SunburstDiagram/static/js/runtime-main.2bc3c3ab.js"
+  },
+  {
+    "revision": "3b962ca2094a75d78cb7",
+    "url": "/chepa17/SunburstDiagram/static/js/xlsx.6f727fbd.chunk.js"
+  }
+]);
---
+++ static/js/xlsx.6f727fbd.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[2],{212:function(p,n){},302:function(p,n){},303:function(p,n){}}]);
+//# sourceMappingURL=xlsx.6f727fbd.chunk.js.map
